9a2c6843374b5c64c43b3a5663ad20aec7b74bb4
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2024-12-20 10:13:07 +0100

    create and style the upload avatar section

---
+++ src/components/UploadAvatar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import uploadIcon from "../assets/images/icon-upload.svg";
+
+function UploadAvatar() {
+  return (
+    <div>
+      <h3 className=" text-[#fff] font-Inconsolata text-xl">Upload Avatar</h3>
+      <DragDrop />
+      <p className=" text-[#fff] font-Inconsolata text-xs">
+        <span>{}</span>
+        Upload your photo (JPG or PNG, max size: 500KB).
+      </p>
+    </div>
+  );
+}
+
+function DragDrop() {
+  return (
+    <div className=" my-2 bg-[#81797931] border-dashed border-[#ffffff6f] border-2 h-32 w-70 rounded-md flex flex-col items-center justify-between p-4">
+      <div className=" p-3 bg-[#524b4b5d] rounded-md">
+        <img className=" h-8 w-8" src={uploadIcon} alt="upload icon" />
+      </div>
+      <p className=" text-[#fff] font-Inconsolata">
+        Drag and drop or click to upload
+      </p>
+    </div>
+  );
+}
+
+export default UploadAvatar;
